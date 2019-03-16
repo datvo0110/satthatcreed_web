9bcbd8c49ee00a53d37a93789cf182ebf3b08b65
datvo0110 <datvo0110@gmail.com>
2019-03-16 08:16:25 +0800

    done

--- next.config.js
+++ next.config.js
@@ -1,23 +1,31 @@
-module.exports = {
-  webpack: (config, { defaultLoaders }) => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'emit-file-loader',
-          options: {
-            name: 'dist/[path][name].[ext]',
-          },
-        },
-        'babel-loader',
-        'styled-jsx-css-loader',
-      ],
-    })
+const withSass = require('@zeit/next-sass');
+const withCSS = require('@zeit/next-css');
+const commonsChunkConfig = require('@shuta/commons-chunk-config');
 
-    return config
-  }
-}
+module.exports = withSass(withCSS({
+  webpack: config => commonsChunkConfig(config, /\.(sass|scss|css)$/)
+}));
+
+// module.exports = {
+//   webpack: (config, { defaultLoaders }) => {
+//     // Fixes npm packages that depend on `fs` module
+//     config.node = {
+//       fs: 'empty'
+//     }
+//     config.module.rules.push({
+//       test: /\.scss$/,
+//       use: [
+//         {
+//           loader: 'emit-file-loader',
+//           options: {
+//             name: 'dist/[path][name].[ext]',
+//           },
+//         },
+//         'babel-loader',
+//         'styled-jsx-css-loader',
+//       ],
+//     })
+//
+//     return config
+//   }
+// }
